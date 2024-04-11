89fe5b140eea1ca0eedfb8e85a525e91877785a3
KAuser2094 <kauser2094@gmail.com>
2024-04-11 17:06:39 +0100

    Change reactor to see cells

--- reactor/reactor.lua
+++ reactor/reactor.lua
@@ -3,15 +3,14 @@
   i.e. a durability of 0.75 means only 25% is left (10k*25% is 2.5k)
   You can loop and check for any that are below (above a number) threshold durability and stop the reactor. Then simply dump out and in old and new condensators.
 ]]
+local invFunc = require("KA_CC_Programs.inventory.inventory")
 local chamber = peripheral.wrap("back")
 local reactor = chamber.getReactorCore()
 local inv = reactor.list()
-local invAsStr = textutils.serialise(inv)
---textutils.pagedPrint(invAsStr)
-local itemMeta1 = textutils.serialise(reactor.getItemMeta(1))
-print(itemMeta1)
-local itemMeta22 = textutils.serialise(reactor.getItemMeta(22))
-print(itemMeta22)
+local itemMeta2 = textutils.serialise(reactor.getItemMeta(2))
+print(itemMeta2)
+local itemMeta13 = textutils.serialise(reactor.getItemMeta(13))
+print(itemMeta13)
 
 print("Press any key to finish")
 local event, key = os.pullEvent("key")
