f5e9ed066947f3dc12ee26e8ff8e8685922e0567
KAuser2094 <kauser2094@gmail.com>
2024-04-12 11:43:48 +0100

    Why does this have the most methods?

---
+++ logistics_pipes/Doc/itemSinkMod.lua
@@ -0,0 +1,36 @@
+return {
+  commandHelp = {
+    "---------------------------------\
+Command: getFilterInventory\
+Parameter: NONE\
+Return Type: net.minecraft.inventory.IInventory\
+Description: \
+Returns the FilterInventory of this Module",
+    "---------------------------------\
+Command: getFilterInventory\
+Parameter: NONE\
+Return Type: network.rs485.logisticspipes.inventory.IItemIdentifierInventory\
+Description: \
+Returns the FilterInventory of this Module",
+    "---------------------------------\
+Command: setDefaultRoute\
+Parameter: \
+Boolean\
+Return Type: void\
+Description: \
+Sets the default route status of this module",
+    "---------------------------------\
+Command: isDefaultRoute\
+Parameter: NONE\
+Return Type: boolean\
+Description: \
+Returns true if the module is a default route",
+    "---------------------------------\
+Command: hasGui\
+Parameter: NONE\
+Return Type: boolean\
+Description: \
+Returns true if the Pipe has a gui",
+    "No command with that index",
+  },
+}
