2959677ef073f84b87b4d4cbeefa70629c522c73
KAuser2094 <kauser2094@gmail.com>
2024-04-12 11:55:37 +0100

    helpful

---
+++ random_functions_to_remember.lua
@@ -0,0 +1,15 @@
+-- Just some random functions that I often use
+
+-- TRY REQUIRE (will keep trying different paths and only fail after trying all)
+local function tryRequire(paths)
+	local errorMsgs = {}
+	for _, path in ipairs(paths) do
+		local success, module = pcall(require, path)
+		if success then
+			return module
+		else
+			table.insert(errorMsgs, "Failed to require from '" .. path .. "': " .. module)
+		end
+	end
+	error(table.concat(errorMsgs, "\n"))
+end
