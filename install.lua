57dfe06d23fc4e18bf9ac6e69798f480115f8d98
KAuser2094 <kauser2094@gmail.com>
2024-04-12 13:37:48 +0100

    wget get doesn't make sense, removed dupe

--- install.lua
+++ install.lua
@@ -1,2 +1,2 @@
-shell.run("wget get https://raw.githubusercontent.com/KAuser2094/KA_CC_Programs/master/git.lua")
+shell.run("wget https://raw.githubusercontent.com/KAuser2094/KA_CC_Programs/master/git.lua")
 shell.run("git clone KAuser2094 KA_CC_Programs")
