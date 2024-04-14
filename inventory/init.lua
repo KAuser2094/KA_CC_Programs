eb520bd41cf0970acc805fa93c7dc6543a1b42bf
KAuser2094 <kauser2094@gmail.com>
2024-04-14 19:25:21 +0100

    Change inventory to use oop like structure

--- inventory/init.lua
+++ inventory/init.lua
@@ -1,43 +1,75 @@
 -- Define a table to store the module functions
-local inventory = {}
+local module = {}
 
-local function checkIfInv(invTable)
-	if invTable.list then
-		return true
-	else
-		print("invTable: " .. invTable.getMetadata["name"] .. " may not be an inventory, missing methods")
-		return false
-	end
+-- BETTER INVENTORY: Basically a different api when working with inventory peripherals, wraps around the normal stuff and gives extra functionality
+local betterInventory = {}
+
+betterInventory.needsSideSpecified = { "ic2" }
+
+-- Original Inventory API functions:
+-- Look at original api wiki
+function betterInventory:size()
+	return self.api.size()
 end
 
--- Takes in: Inventory Block Table, Slot number, Key in the item meta to read.
--- Returns: The value to the key in the item at the slot given or nil if failure.
+-- Look at original api wiki
+function betterInventory:list()
+	return self.api.list()
+end
 
-function inventory.getValueAtInv_Slot_MetaKey(invTable, slot, metakey)
-	if not checkIfInv(invTable) then
-		return
-	end
-	local itemMeta = invTable.getItemMeta(slot)
-	local val_or_nil = itemMeta[metakey]
-	if val_or_nil == nil then
-		print("Key: " .. metakey .. " did not exist")
-	end
+-- Look at original api wiki
+function betterInventory:getItemDetail(slot)
+	return self.api.getItemDetail(slot)
+end
+
+-- Look at original api wiki
+function betterInventory:getItemLimit(slot)
+	return self.api.getItemDetail(slot)
+end
+
+-- Look at original api wiki
+function betterInventory:pushItems(toName, fromSlot, limit, toSlot)
+	-- Yes these seem a bit redundant
+	limit = limit or nil
+	toSlot = toSlot or nil
+	return self.api.pushItems(toName, fromSlot, limit, toSlot)
+	-- TODO: Add check for mod type and fix for ic2
+end
+
+-- Look at original api wiki
+function betterInventory:pullItems(fromName, fromSlot, limit, toSlot)
+	-- Yes these seem a bit redundant
+	limit = limit or nil
+	toSlot = toSlot or nil
+	return self.api.pullItems(fromName, fromSlot, limit, toSlot)
+	-- TODO: Add check for mod type and fix for ic2
+end
+-- End of original inventory API
+
+-- The known insides of the inventory are only updated when this is called. Most functions use the api directly, however.
+function betterInventory:refreshContent()
+	self.content = self.api.list()
+end
+
+-- @param key - what key in the item meta whose value you want to retrieve.
+-- @param slot - What slot in the inventory to get value from.
+-- @return The value, or nil if not found.
+function betterInventory:getMetaDataValueAtKeyAtSlot(key, slot)
+	local itemMeta = self.api.getItemDetail(slot)
+	local val_or_nil = itemMeta[key]
 	return val_or_nil
 end
 
--- Takes in: Inventory Block Table, function that takes in an item metadata and return true or false
--- Optional: Start Range, End Range
--- Returns: List with slots of items that fulfil the criteria
-
-function inventory.findItemsInInvThatFulfilsFunction(invTable, lowerFunction, startRange, endRange)
-	if not checkIfInv(invTable) then
-		return
-	end
+-- @param lowerFunction - This should take in an item's meta data and return either true if you want to keep it, or false to discard.
+-- @param (opt.) startRange - At what slot in inventory to start search. (Default is 1)
+-- @param (opt.) endRange - At what slot in inventory to end search. (Default is `.size()` of api)
+-- @return A list of the inventory slots that fulfil the `lowerFunction`.
+function betterInventory:findItemsThatFulfilsFunction(lowerFunction, startRange, endRange)
 	startRange = startRange or 1
-	endRange = endRange or invTable.size()
+	endRange = endRange or self.api.size()
 	local indexList = {}
 	for slot = startRange, endRange do
-		local itemMeta = invTable.getItemMeta(slot)
+		local itemMeta = self.api.getItemDetail(slot)
 		if lowerFunction(itemMeta) then
 			table.insert(indexList, slot)
 		end
@@ -45,20 +77,39 @@ function inventory.findItemsInInvThatFulfilsFunction(invTable, lowerFunction, st
 	return indexList
 end
 
--- Takes in: Inventory Block Table, Value for comparison, Key of value.
--- Optional: Start Range, End Range
--- Returns: List with slots of items that fulfil the criteria
-
-function inventory.findItemsInInvWith_Value_AtMetaKey(invTable, value, metaKey, startRange, endRange)
-	if not checkIfInv(invTable) then
-		return
-	end
+-- @param value - What value you want to find
+-- @param metakey - What key in the metadata to look for value in
+-- @param (opt.) startRange - At what slot in inventory to start search. (Default is 1)
+-- @param (opt.) endRange - At what slot in inventory to end search. (Default is `.size()` of api)
+-- @return A list of the inventory slots where the `value` was found at the `metakey` key in the item metadata.
+function betterInventory:findItemsWithMetaDataValueAtKey(value, metaKey, startRange, endRange)
 	local lowerFunction = function(inputItemMeta)
 		return value == itemMeta[metakey]
 	end
 	startRange = startRange or 1
-	endRange = endRange or invTable.size()
-	return inventory.findItemsInInvThatFulfilsFunction(invTable, lowerFunction, startRange, endRange)
+	endRange = endRange or self.api.size()
+	return self:findItemsThatFulfilsFunction(lowerFunction, startRange, endRange)
 end
 
-return inventory
+function module.createBetterInventory(networkName)
+	local instance = {
+		name = networkName,
+		api = peripheral.wrap(networkName),
+		type = peripheral.getType(networkName),
+		mod = networkName:match("([^:_]+)"),
+	}
+	if not instance.api then
+		print(instance.name .. " is not an available peripheral, cannot create a betterInventory instance with it")
+	end
+	if not instance.api.list then
+		print(instance.name .. " is not an inventory, cannot create a betterInventory instance with it")
+		return nil
+	end
+	instance.content = instance.api.list()
+	setmetatable(instance, { __index = betterInventory })
+
+	return instance
+end
+-- END OF BETTER INVENTORY
+
+return module
--- testing.lua
+++
@@ -1,5 +0,0 @@
-local obs = peripheral.warp("minecraft:ironchest_obsidian_1")
-local reactors = { peripheral.find("ic2:nuclear") }
-for _, reactor in ipairs(reactors) do
-	print(peripheral.getName(reactor))
-end
