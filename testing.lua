a4b414f5929c4cb8ba9cafca33c71962e7be570c
KAuser2094 <kauser2094@gmail.com>
2024-04-14 12:28:31 +0100

    Try out reactors again

--- testing.lua
+++ testing.lua
@@ -1,6 +1,5 @@
-local netNameOrSide = "back"
-local requester = peripheral.wrap("back")
-local available = requester.getAvailableItems()
-local II_1_Stack = available[1]
-local II_1 = II_1_Stack.getValue1()
-requester.makeRequest(II_1_Stack)
+local obs = peripheral.warp("minecraft:ironchest_obsidian_1")
+local reactors = { peripheral.find("ic2:nuclear") }
+for _, reactor in ipairs(reactors) do
+	print(peripheral.getName(reactor))
+end
