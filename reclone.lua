65ff7929ca863217eb3b0a0628377de82f089d30
KAuser2094 <kauser2094@gmail.com>
2024-04-14 10:42:16 +0100

    add reclone script

---
+++ reclone.lua
@@ -0,0 +1,3 @@
+-- Reclones the program using `(repo_folder)/_git/redoCloneTree.lua`
+shell.run("wget https://raw.githubusercontent.com/KAuser2094/KA_CC_Programs/master/git.lua")
+shell.run("git reclone KA_CC_Programs")
