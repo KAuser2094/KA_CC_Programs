cb1a86b55fe2777e3bc0813c06e47e8324f55c84
KAuser2094 <kauser2094@gmail.com>
2024-04-14 10:44:42 +0100

    just try and see what the key values are

--- testing.lua
+++ testing.lua
@@ -1,11 +1,6 @@
 local netNameOrSide = "back"
 local requester = peripheral.wrap("back")
 local available = requester.getAvailableItems()
-for _, item in ipairs(available) do
-	-- Extract information about the item
-	local itemName = item.getName()
-	local itemObject = item.getObject()
-
-	-- Do whatever you need with the item information
-	print("Item: " .. itemName .. ", Object: " .. itemObject)
+for key, value in pairs(available) do
+	print("Key:", key, "Value:", value)
 end
