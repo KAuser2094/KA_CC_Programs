5246f115ded114a696c213965672c6d2cdf6c68f
KAuser2094 <kauser2094@gmail.com>
2024-04-14 10:40:36 +0100

    Try and test for itemIndertifier

--- testing.lua
+++ testing.lua
@@ -1,5 +1,11 @@
 local netNameOrSide = "back"
 local requester = peripheral.wrap("back")
 local available = requester.getAvailableItems()
-local serialised = textutils.serialise(available)
-textutils.pagedPrint(serialised)
+for _, item in ipairs(available) do
+	-- Extract information about the item
+	local itemName = item.getName()
+	local itemObject = item.getObject()
+
+	-- Do whatever you need with the item information
+	print("Item: " .. itemName .. ", Object: " .. itemObject)
+end
