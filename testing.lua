98251bed07c091cf708704b7f6bfa77e1ec3c1dd
KAuser2094 <kauser2094@gmail.com>
2024-04-14 10:48:07 +0100

    test itemidentifier itself

--- testing.lua
+++ testing.lua
@@ -1,6 +1,7 @@
 local netNameOrSide = "back"
 local requester = peripheral.wrap("back")
 local available = requester.getAvailableItems()
-for key, value in pairs(available) do
+local ItemIdentifier1 = available[1]
+for key, value in pairs(ItemIdentifier1) do
 	print("Key:", key, "Value:", value)
 end
