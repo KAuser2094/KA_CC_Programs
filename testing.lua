fa8af4ca57d304fcb1508ea00b282cec33545fe6
KAuser2094 <kauser2094@gmail.com>
2024-04-15 10:04:24 +0100

    Get rid of unecessary test

--- testing.lua
+++ testing.lua
@@ -21,8 +21,6 @@ local reactors = invModule.convertInventoryListToBetterInventoryList(reactors)
 for i, reactor in ipairs(reactors) do
 	print(reactor.name)
 	reactor:setConnectionSide("up")
-	print(reactor.size())
-	-- print(reactor.size(reactor))
 	print(reactor:size())
 
 	-- Tests:
