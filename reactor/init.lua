e3a84766531e8004f0204d03a6a83a24c53389d8
KAuser2094 <kauser2094@gmail.com>
2024-04-15 10:23:24 +0100

    Add create methods

--- reactor/init.lua
+++ reactor/init.lua
@@ -20,11 +20,52 @@ local inv = tryRequire({ "inventory", "KA_CC_Programs/inventory", "../inventory"
 -- Store modules
 local module = {}
 
+-- BETTER REACTOR: Basically betterInventory but with some extra stuff that is more specific.
 local betterReactor = inv.getCopyOfBetterInventoryDefinitionTable() -- "Inherits" fields and methods
 
 -- @return { "KA_betterReactor", "KA_betterInventory" }
 function KA_betterReactor.getClassTypes()
 	return { "KA_betterReactor", "KA_betterInventory" }
 end
+
+function module.createBetterReactor(networkName)
+	local instance = {
+		name = networkName,
+		api = peripheral.wrap(networkName),
+		type = peripheral.getType(networkName),
+		mod = networkName:match("([^:_]+)"),
+	}
+	if not instance.api then
+		print(instance.name .. " is not an available peripheral, cannot create a betterInventory instance with it")
+	end
+	if not instance.api.list then
+		print(instance.name .. " is not an inventory, cannot create a betterInventory instance with it")
+		return nil
+	end
+	instance.content = instance.api.list()
+	instance.verbosity = 0
+	setmetatable(instance, { __index = betterReactor })
+	instance:getAPIFunctions()
+
+	return instance
+end
+
+function module.convertReactorToBetterReactor(inventoryApi)
+	return module.createBetterReactor(peripheral.getName(inventoryApi))
+end
+
+function module.convertReactorListToBetterReactorList(inventoryApiList)
+	local newList = {}
+	for i, inventoryApi in ipairs(inventoryApiList) do
+		newList[i] = module.convertReactorToBetterReactor(inventoryApi)
+	end
+	return newList
+end
+
+function module.getCopyOfBetterReactorDefinitionTable()
+	return shallowCopy(betterReactor)
+end
+
+-- END OF BETTER REACTOR
 
 return module
