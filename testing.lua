bab115f213d483de5076159c05d34969042e6134
KAuser2094 <kauser2094@gmail.com>
2024-04-15 10:24:29 +0100

    Try and test for betterReactors

--- testing.lua
+++ testing.lua
@@ -17,7 +17,7 @@ local oN = "minecraft:ironchest_obsidian_1"
 local o = invModule.createBetterInventory(oN)
 
 local reactors = { peripheral.find("ic2:nuclear reactor") }
-local reactors = invModule.convertInventoryListToBetterInventoryList(reactors)
+local reactors = invModule.convertReactorListToBetterReactorList(reactors)
 for i, reactor in ipairs(reactors) do
 	print(reactor.name)
 	reactor:setConnectionSide("up")
