35ac76f0cea3064d221c53bca5af49403ac95c84
KAuser2094 <kauser2094@gmail.com>
2024-04-19 09:15:37 +0100

    Fix

--- ic2/emc_farm.lua
+++ ic2/emc_farm.lua
@@ -35,11 +35,13 @@ end
 
 local function pullFromMacerators()
 	for _, macerator in ipairs(macerators) do
-		macerator:pushItems(condensorRM, 3)
+		macerator:pushItems(condensorRM, 2) -- Who knows why this is 2 when pulling/pushing but is actually in slot 3
 	end
 end
 
 for i = 1, 5 do
+	print("Pushing")
 	pushToMacerators() -- This has an os.sleep(1)
+	print("Pulling")
 	pullFromMacerators()
 end
