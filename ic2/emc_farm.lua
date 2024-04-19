809cfbaa680b90293b8e989d56dc5d2c4751f76c
KAuser2094 <kauser2094@gmail.com>
2024-04-19 09:17:11 +0100

    For some reason it only ever pushes to slot 1

--- ic2/emc_farm.lua
+++ ic2/emc_farm.lua
@@ -34,8 +34,8 @@ local function pushToMacerators() -- Does 21 stacks
 end
 
 local function pullFromMacerators()
-	for _, macerator in ipairs(macerators) do
-		macerator:pushItems(condensorRM, 2) -- Who knows why this is 2 when pulling/pushing but is actually in slot 3
+	for i, macerator in ipairs(macerators) do
+		macerator:pushItems(condensorRM, 2, nil, i) -- Who knows why this is 2 when pulling/pushing but is actually in slot 3
 	end
 end
 
