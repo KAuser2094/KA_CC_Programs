6bb823a130484d218651f53b2ab541a6f61ce35f
KAuser2094 <kauser2094@gmail.com>
2024-12-19 07:35:56 +0000

    Forgot to change the raw link

--- simple_install.lua
+++ simple_install.lua
@@ -1,3 +1,3 @@
 -- FROM OLD REPO, SHOULD BE CHANGED OUT LATER.
-shell.run("wget https://raw.githubusercontent.com/KAuser2094/KA_CC_Programs/master/git.lua")
-shell.run("git clone KAuser2094 KA_CC_Programs KA_CC")
+shell.run("wget https://raw.githubusercontent.com/KAuser2094/KA_CC_Programs/refs/heads/master/simple_git.lua")
+shell.run("simple_git clone KAuser2094 KA_CC_Programs KA_CC")
