b17afa22a1d65cee09ffeee0d8a09ee5fe3f9599
KAuser2094 <kauser2094@gmail.com>
2024-12-20 11:39:59 +0000

    Move into tests

--- tests/run_tests.lua
+++ tests/run_tests.lua
@@ -1,6 +1,6 @@
 -- Runs tests is KA_CC.tests
 local Test = require "KA_CC.tests"
-local context = require "KA_CC.test_context"
+local context = require "KA_CC.tests.test_context"
 -- (maybe) use passed in parameters in terminal to change context (like verbosity and waiting for input before next test)
 local waitForInput = true
 -- Get test class
